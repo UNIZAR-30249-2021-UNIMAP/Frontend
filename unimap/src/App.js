298963fbec5d95861d0c64c685aab0b986c67855
SergioGCH <61270404+SergioGCH@users.noreply.github.com>
2021-03-23 16:06:50 +0100

    Implementada bara superior de navegación

--- unimap/src/App.js
+++ unimap/src/App.js
@@ -1,13 +1,6 @@
 import React, {Component} from "react";
-import logo from './logo.svg';
 import './App.css';
-
-import {BrowserRouter}  from "react-router-dom";
-import {Route} from "react-router-dom";
-import {Switch} from "react-router-dom";
-//import {Link} from "react-router-dom"; 
-import {Redirect} from "react-router-dom";
-import { render } from '@testing-library/react';
+import NavBar from "./Components/NavBar/NavBar";
 
 
 //Pages
@@ -18,20 +11,8 @@ export default class App extends Component {
   render(){
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-         <BrowserRouter> 
-            <Switch>
-              <Route exact path="/" component={MainPage_hu6}/>
-              <Route exact path="/404" component={NotFoundPage}/>
-              <Redirect to="/404"/>
-            </Switch>
-         </BrowserRouter>
-        </header>
-      </div>
+        <NavBar />
+      </div>     
     );
   }
 }
