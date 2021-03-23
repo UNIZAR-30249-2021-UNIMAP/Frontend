b0b429469419dbd1bd0d8b769c7589fe0a2d94fa
Alex28499 <a.terrona@hotmail.es>
2021-03-23 12:45:24 +0100

    Prueba rutas y bibliotecas

--- unimap/src/App.js
+++ unimap/src/App.js
@@ -1,25 +1,37 @@
+import React, {Component} from "react";
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import {BrowserRouter}  from "react-router-dom";
+import {Route} from "react-router-dom";
+import {Switch} from "react-router-dom";
+//import {Link} from "react-router-dom"; 
+import {Redirect} from "react-router-dom";
+import { render } from '@testing-library/react';
 
-export default App;
+
+//Pages
+import MainPage_hu6 from "./pages";
+import NotFoundPage from "./pages/404";
+
+export default class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+         <BrowserRouter> 
+            <Switch>
+              <Route exact path="/" component={MainPage_hu6}/>
+              <Route exact path="/404" component={NotFoundPage}/>
+              <Redirect to="/404"/>
+            </Switch>
+         </BrowserRouter>
+        </header>
+      </div>
+    );
+  }
+}
---
+++ unimap/src/pages/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+const MainPage_hu6 = () => {
+    return(
+    <div>
+        <h3>Welcome to React Router</h3> 
+        <small>Main page</small>
+    </div>
+    );
+}
+
+export default MainPage_hu6;
